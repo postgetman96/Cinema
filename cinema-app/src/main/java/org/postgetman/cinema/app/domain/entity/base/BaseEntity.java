c38e4e456285949dc228e9efa094afb793b4df01
postgetman <postgetman@gmail.com>
2017-07-27 15:00:52 +0300

    Add BaseEntity.java file

---
+++ cinema-app/src/main/java/org/postgetman/cinema/app/domain/entity/base/BaseEntity.java
@@ -0,0 +1,10 @@
+package org.postgetman.cinema.app.domain.entity.base;
+
+public abstract class BaseEntity {
+
+    private Long id;
+
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
+}

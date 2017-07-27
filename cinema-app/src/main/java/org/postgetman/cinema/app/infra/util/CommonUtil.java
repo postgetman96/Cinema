d4ef9377606fbc197711861d7d9324057f25cf07
postgetman <postgetman@gmail.com>
2017-07-27 15:02:47 +0300

    Add BaseEntity.java file

---
+++ cinema-app/src/main/java/org/postgetman/cinema/app/infra/util/CommonUtil.java
@@ -0,0 +1,30 @@
+package org.postgetman.cinema.app.infra.util;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+public class CommonUtil {
+
+    private CommonUtil() {
+    }
+
+    /**
+     * Returns not-null unmodifiable copy of the source set
+     * @param source
+     * @return
+     */
+    public static <T> Set<T> getSafeSet(Set<T> source) {
+        return Collections.unmodifiableSet(Optional.ofNullable(source).orElse(Collections.emptySet()));
+    }
+
+    /**
+     * Returns not-null unmodifiable copy of the source list
+     * @param source
+     * @return
+     */
+    public static <T> List<T> getSafeList(List<T> source) {
+        return Collections.unmodifiableList(Optional.ofNullable(source).orElse(Collections.emptyList()));
+    }
+}

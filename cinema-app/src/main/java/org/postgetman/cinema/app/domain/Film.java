0b4ec98e4db7637cedc4039becb9785fb37224f0
postgetman <postgetman@gmail.com>
2017-07-27 15:56:53 +0300

    Add  another Exception files

--- cinema-app/src/main/java/org/postgetman/cinema/app/domain/Film.java
+++ cinema-app/src/main/java/org/postgetman/cinema/app/domain/Film.java
@@ -2,6 +2,33 @@ package org.postgetman.cinema.app.domain;
 
 import org.postgetman.cinema.app.domain.entity.base.BaseEntity;
 
+
 public class Film extends BaseEntity{
+
+    private String name;
+
+    private String genre;
+
+    private String producer;
+
+    private int duration;
+
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getGenre() { return genre; }
+
+    public void setGenre(String genre) { this.genre = genre; }
+
+    public String getProducer() { return producer; }
+
+    public void setProducer(String producer) { this.producer = producer; }
+
+    public int getDuration() { return duration; }
+
+    public void setDuration(int duration) { this.duration = duration; }
+
 
 }
--- cinema-app/src/main/java/org/postgetman/cinema/app/domain/Ticket.java
+++ cinema-app/src/main/java/org/postgetman/cinema/app/domain/Ticket.java
@@ -1,4 +1,34 @@
 package org.postgetman.cinema.app.domain;
 
-public class Ticket {
+import org.postgetman.cinema.app.domain.entity.base.BaseEntity;
+
+public class Ticket extends BaseEntity{
+
+    private Long filmId;
+
+    private int row;
+
+    private int place;
+
+    private int price;
+
+
+
+    public Long getFilmId() { return filmId; }
+
+    public void setFilmId(Long filmId) { this.filmId = filmId; }
+
+    public int getRow() { return row; }
+
+    public void setRow(int row) { this.row = row; }
+
+    public int getPlace() { return place; }
+
+    public void setPlace(int place) { this.place = place; }
+
+    public int getPrice() { return price; }
+
+    public void setPrice(int price) { this.price = price; }
+
+
 }

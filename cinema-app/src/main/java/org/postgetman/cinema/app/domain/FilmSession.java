39d8156ea6698e5225082bf0ef2c6b7d4c7f8cc8
postgetman <postgetman@gmail.com>
2017-07-27 16:08:18 +0300

    add Film session fields

--- cinema-app/src/main/java/org/postgetman/cinema/app/domain/FilmSession.java
+++ cinema-app/src/main/java/org/postgetman/cinema/app/domain/FilmSession.java
@@ -1,4 +1,26 @@
 package org.postgetman.cinema.app.domain;
 
-public class FilmSession {
+import org.postgetman.cinema.app.domain.entity.base.BaseEntity;
+
+import java.sql.Date;
+import java.sql.Time;
+
+public class FilmSession extends BaseEntity{
+
+    private Date date;
+
+    private Film film;
+
+    private Hall hall;
+
+    private Time time;
+
+    private int currentNumberOfTickets;
+
+
+    public Date getDate() { return date; }
+
+    public void setTime(Time time) { this.time = time; }
+
+
 }

dfd5d5c558ddd484c6217238386522913239eb36
postgetman <postgetman@gmail.com>
2017-07-28 12:07:47 +0300

    add FilmService,FilmServiceTest

--- cinema-core/src/main/java/org/postgetman/cinema/app/domain/criteria/FilmSessionCriteria.java
+++ cinema-core/src/main/java/org/postgetman/cinema/app/domain/criteria/FilmSessionCriteria.java
@@ -1,4 +1,24 @@
 package org.postgetman.cinema.app.domain.criteria;
 
+import org.postgetman.cinema.app.domain.Film;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
 public class FilmSessionCriteria {
+
+    private Film film;
+
+    private LocalDate date;
+
+    public static FilmSessionCriteria byDate(LocalDate date){
+        return new FilmSessionCriteria(date);
+    }
+
+    public FilmSessionCriteria(){
+
+    }
+    public FilmSessionCriteria(LocalDate date){
+        this.date = Objects.requireNonNull(date);
+    }
 }

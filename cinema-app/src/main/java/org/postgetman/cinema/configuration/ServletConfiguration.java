4c2cd941e0204bc4cd45628b0fc87ea9cfced1e7
postgetman <postgetman@gmail.com>
2017-07-28 18:32:00 +0300

    add CheckTest.java

--- cinema-app/src/main/java/org/postgetman/cinema/configuration/ServletConfiguration.java
+++ cinema-app/src/main/java/org/postgetman/cinema/configuration/ServletConfiguration.java
@@ -2,12 +2,21 @@ package org.postgetman.cinema.configuration;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "org.postgetman.cinema.app.controller")
+@Import(SwaggerConfiguration.class)
 public class ServletConfiguration extends WebMvcConfigurerAdapter{
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 }

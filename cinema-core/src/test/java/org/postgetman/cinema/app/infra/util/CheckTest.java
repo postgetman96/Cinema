36f112f8b876e3e7f86bdff2b3d0961d09305561
postgetman <postgetman@gmail.com>
2017-07-28 14:17:25 +0300

    add CheckTest.java

---
+++ cinema-core/src/test/java/org/postgetman/cinema/app/infra/util/CheckTest.java
@@ -0,0 +1,29 @@
+package org.postgetman.cinema.app.infra.util;
+
+
+
+import org.postgetman.cinema.app.infra.exception.flow.InvalidParameterException;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+public class CheckTest {
+
+    @Test
+    public void testCheckParameterGetException() {
+        try {
+            Checks.checkParameter(false, "test");
+            assertTrue(false);
+        } catch (Exception ex) {
+            assertSame(ex.getClass(), InvalidParameterException.class);
+            assertEquals(ex.getMessage(), "test");
+        }
+    }
+    @Test
+    public void testCheckParameterNoException() {
+        Checks.checkParameter(true, "test");
+        assertTrue(true);
+    }
+}

517361dd3f41438c7773bfccb84095d11e8f3e55
postgetman <postgetman@gmail.com>
2017-08-01 14:24:18 +0300

    move service.log to tomcat/logs

--- cinema-core/src/main/java/org/postgetman/cinema/app/service/impl/FilmServiceImpl.java
+++ cinema-core/src/main/java/org/postgetman/cinema/app/service/impl/FilmServiceImpl.java
@@ -1,18 +1,19 @@
 package org.postgetman.cinema.app.service.impl;
 
 
-import org.apache.logging.log4j.LogManager;
 import org.postgetman.cinema.app.domain.Film;
 import org.postgetman.cinema.app.infra.util.CommonUtil;
 import org.postgetman.cinema.app.service.FilmService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
-import org.apache.logging.log4j.Logger;
 
 @Service
 public class FilmServiceImpl implements FilmService {
-    private static final Logger LOGGER= LogManager.getLogger(FilmSessionServiceImpl.class);
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FilmSessionServiceImpl.class);
 
     private List<Film> films;
 
--- cinema-core/src/main/java/org/postgetman/cinema/app/service/impl/FilmSessionServiceImpl.java
+++ cinema-core/src/main/java/org/postgetman/cinema/app/service/impl/FilmSessionServiceImpl.java
@@ -1,37 +1,8 @@
 package org.postgetman.cinema.app.service.impl;
 
-import org.postgetman.cinema.app.domain.FilmSession;
-import org.postgetman.cinema.app.infra.util.CommonUtil;
-import org.postgetman.cinema.app.service.FilmSessionService;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Service
-public class FilmSessionServiceImpl implements FilmSessionService{
+public class FilmSessionServiceImpl {
 
-    private List<FilmSession> filmSessions;
-
-    public FilmSessionServiceImpl(){
-        filmSessions = new ArrayList<>();
-    }
-
-    @Override
-    public List<FilmSession> findFilmSessions(){
-        return CommonUtil.getSafeList(filmSessions);
-    }
-
-    public void addFilmSession(FilmSession filmSession){
-        if(!filmSessions.contains(filmSession)){
-            filmSessions.add(filmSession);
-        }
-    }
-
-    @Override
-    public void removeFilmSession(FilmSession filmSession){
-        if(filmSessions.contains(filmSession)){
-            filmSessions.remove(filmSession);
-        }
-    }
 }
